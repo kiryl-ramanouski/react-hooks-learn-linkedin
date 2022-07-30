6e548d504444777d1b74cf1925e227f68f24d8d2
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-30 12:49:51 +0300

    feat: use useState() hook

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Styles
 import './index.css';
 
 const App = () => {
+  const [status, setStatus] = useState('Not delivered');
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>The package is: {status}</h1>
+      <button onClick={() => setStatus('Delivered')}>Delivered</button>
     </div>
   );
 };
