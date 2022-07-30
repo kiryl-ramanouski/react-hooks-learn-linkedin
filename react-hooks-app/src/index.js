bde7aa78a02ef1cf01ee594eb42a69b40a423ba8
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-30 12:39:58 +0300

    refactor: clear index.js

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Styles
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const people = ['Alex', 'Ali', 'Anna'];
+
 root.render(
   <React.StrictMode>
-    <App names={people} />
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
