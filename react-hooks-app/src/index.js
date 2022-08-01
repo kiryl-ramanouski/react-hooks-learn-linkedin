664407406c1aaa0cbfc0c56d71ee27ed5d5224bf
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 12:40:47 +0300

    refactor: clear App

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -1,43 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Styles
 import './index.css';
 
-const App = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.github.com/users')
-      .then((response) => response.json())
-      .then((response) => setData(response));
-  }, []);
-
-  if (data) {
-    return (
-      <>
-        <ul>
-          {data.map((user) => (
-            <li key={user.id}>{user.login}</li>
-          ))}
-        </ul>
-        <button
-          onClick={() => {
-            setData([]);
-          }}
-        >
-          Remove data
-        </button>
-      </>
-    );
-  }
-
-  return (
-    <div>
-      <p>No users</p>
-    </div>
-  );
-};
+const App = () => {};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
