eb701826299d39cefb224dbfbdce2afb82021862
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 12:49:14 +0300

    feat: use useReducer() hook for simple controlled checkbox. useResucer reduce potential bugs

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom/client';
 // Styles
 import './index.css';
 
-const App = () => {};
+const App = () => {
+  const [checked, toggle] = useReducer((checked) => !checked, false);
+  return (
+    <div>
+      <label>
+        <input type='checkbox' value={checked} onChange={toggle} />
+        {checked ? 'checked' : 'unchecked'}
+      </label>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
