88a3a25146fcae093ac95dbba1bf913a20c554a7
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 13:08:39 +0300

    feat: use useReducer for state management. useReducer good if we want to rely on previous state

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -4,8 +4,36 @@ import ReactDOM from 'react-dom/client';
 // Styles
 import './index.css';
 
+const initialState = {
+  message: 'hi',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'yell':
+      return {
+        message: 'HEY!',
+      };
+    case 'whisper':
+      return {
+        message: 'excuse me',
+      };
+    default:
+      return {
+        message: 'hi',
+      };
+  }
+};
+
 const App = () => {
-  return <div></div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      <p>{state.message}</p>
+      <button onClick={() => dispatch({ type: 'yell' })}>YELL</button>
+      <button onClick={() => dispatch({ type: 'whisper' })}>whisper</button>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
