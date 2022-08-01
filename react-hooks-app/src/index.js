7a325543499e1060eceb54785440e1efb5eb08aa
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 11:52:48 +0300

    feat: add useEffect with fetch (without async). Use api/github,com/users

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -5,7 +5,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 const App = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users')
+      .then((response) => response.json())
+      .then((response) => setData(response));
+  }, [data]);
+
+  return (
+    <div>
+      <p>No users</p>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
