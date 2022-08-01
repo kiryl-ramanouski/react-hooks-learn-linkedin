e694eb9f8f6965fcdbeb4909d4b345003b6a8797
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 12:42:46 +0300

    feat: use useReducer() hook for incriminating. Callback then initial value

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -4,7 +4,24 @@ import ReactDOM from 'react-dom/client';
 // Styles
 import './index.css';
 
-const App = () => {};
+const App = () => {
+  const [number, setNumber] = useReducer(
+    (number, newNumber) => number + newNumber,
+    0
+  );
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button
+        onClick={() => {
+          setNumber(1);
+        }}
+      >
+        Add 1
+      </button>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
