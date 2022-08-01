0737524d6f69ff6beccdb20a9d0c15325fda3532
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 14:00:46 +0300

    feat: use useRef for creating uncontrolled component. Use ref parametr, preventDefault(), ref.current.value.

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -1,11 +1,29 @@
-import React, { useReducer } from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Styles
 import './index.css';
 
 const App = () => {
-  return <></>;
+  const sound = useRef();
+  const color = useRef();
+
+  const submit = (e) => {
+    e.preventDefault();
+    const soundVal = sound.current.value;
+    const colorVal = color.current.value;
+    alert(`${soundVal} sounds like ${colorVal}`);
+    sound.current.value = '';
+    color.current.value = '';
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <input type='text' ref={sound} placeholder='Sound...' />
+      <input type='color' ref={color} />
+      <button>Add</button>
+    </form>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
