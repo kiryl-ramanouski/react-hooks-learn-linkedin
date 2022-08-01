09f1b15aac06c191e66511e49993d5a45c28ade3
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 13:19:00 +0300

    feat: clear App

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -4,36 +4,8 @@ import ReactDOM from 'react-dom/client';
 // Styles
 import './index.css';
 
-const initialState = {
-  message: 'hi',
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'yell':
-      return {
-        message: `HEY! I JUST SAID ${state.message}!`,
-      };
-    case 'whisper':
-      return {
-        message: `excuse me, I just said ${state.message}...`,
-      };
-    default:
-      return {
-        message: 'hi',
-      };
-  }
-};
-
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <div>
-      <p>{state.message}</p>
-      <button onClick={() => dispatch({ type: 'yell' })}>YELL</button>
-      <button onClick={() => dispatch({ type: 'whisper' })}>whisper</button>
-    </div>
-  );
+  return <></>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
