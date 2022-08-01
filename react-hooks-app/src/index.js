82ee77585cd69f337191d6baf6e5be2d06e8b0a1
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 17:13:49 +0300

    refactor: clear app

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Custom hooks
-import useInput from './Hooks/useInput';
-
 // Styles
 import './index.css';
 
 const App = () => {
-  const [soundProps, resetSound] = useInput('');
-  const [colorProps, resetColor] = useInput('#000000');
-
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${soundProps.value} sounds like ${colorProps.value}`);
-    resetSound();
-    resetColor();
-  };
-
-  return (
-    <form onSubmit={submit}>
-      <input type='text' placeholder='Sound...' {...soundProps} />
-      <input type='color' {...colorProps} />
-      <button>Add</button>
-    </form>
-  );
+  return <></>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
