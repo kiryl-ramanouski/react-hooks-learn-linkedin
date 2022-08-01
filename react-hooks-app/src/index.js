569e23e7599f24c436ba44875876baee65daa2c3
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 11:12:55 +0300

    refactor: clear App

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -1,51 +1,14 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-// External components
-import { FaStar } from 'react-icons/fa';
-
 // Styles
 import './index.css';
 
-// Helpers functions
-const createArray = (length) => [...Array(length)];
-
-// Components
-const Star = ({ selected = false, onSelect }) => {
-  return (
-    <FaStar
-      color={selected ? 'red' : 'grey'}
-      onClick={() => onSelect()}
-      cursor='pointer'
-    />
-  );
-};
-
-const StarRating = ({ totalStars = 5 }) => {
-  const [selectedStars, setSelectedStars] = useState(0);
-  return (
-    <>
-      {createArray(totalStars).map((item, i) => (
-        <Star
-          key={i}
-          selected={selectedStars > i}
-          onSelect={() => {
-            setSelectedStars(i + 1);
-          }}
-        />
-      ))}
-      <p>
-        Selected {selectedStars} from {totalStars}
-      </p>
-    </>
-  );
-};
-
 const App = () => {
   return (
-    <>
-      <StarRating totalStars={10} />
-    </>
+    <section>
+      <p>Congratulation!</p>
+    </section>
   );
 };
 
