f65b13bf531edde5c4425abfd2cea2d1755ff0fc
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 12:50:57 +0300

    feat: clear App

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -1,19 +1,11 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Styles
 import './index.css';
 
 const App = () => {
-  const [checked, toggle] = useReducer((checked) => !checked, false);
-  return (
-    <div>
-      <label>
-        <input type='checkbox' value={checked} onChange={toggle} />
-        {checked ? 'checked' : 'unchecked'}
-      </label>
-    </div>
-  );
+  return <div></div>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
