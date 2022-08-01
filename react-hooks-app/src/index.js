f77f98f3b6bbe47afb42630070340da26176d093
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 11:36:30 +0300

    refactor: clear app

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -5,25 +5,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 const App = () => {
-  const [name, setName] = useState('Jan');
-  const [admin, setAdmin] = useState(false);
-
-  useEffect(() => {
-    console.log(`Celebrate ${name}`);
-  }, [name]);
-
-  useEffect(() => {
-    console.log(`The user is: ${admin ? 'admin' : 'not admin'}`);
-  }, [admin]);
-
-  return (
-    <section>
-      <p>Congratulation {name}!</p>
-      <button onClick={() => setName('Will')}>Change winner</button>
-      <p>{admin ? 'logged in' : 'not logged in'}</p>
-      <button onClick={() => setAdmin(true)}>Log in</button>
-    </section>
-  );
+  return <div></div>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
