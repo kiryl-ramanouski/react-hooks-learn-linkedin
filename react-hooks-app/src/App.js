c152a0ed24ad083a0d76c826d190aaad72bd0a08
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 17:45:09 +0300

    feat: use useContext() hook for using global context data from any point of the app.

--- react-hooks-app/src/App.js
+++ react-hooks-app/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+// Global context
+import { TreesContext } from './index';
 
 // Styles
 import './App.css';
 
 const App = () => {
-  return <></>;
+  const { trees, log } = useContext(TreesContext);
+  return (
+    <>
+      <h1 style={{ color: 'red', textAlign: 'center' }}>My trees</h1>
+      <ul>
+        {trees.map((item) => {
+          return <li key={item.id}>{item.type}</li>;
+        })}
+      </ul>
+      <h2 style={{ color: 'red', textAlign: 'center' }}>Use it to get log:</h2>
+      <ul>
+        {log.map((item) => {
+          return <li key={item.id}>{item.type}</li>;
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default App;
