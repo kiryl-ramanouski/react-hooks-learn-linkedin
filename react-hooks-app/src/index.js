da41454eeeb43ab2d377dd5cec74c8f3081fc625
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-01 17:44:06 +0300

    feat: use createContext() hook with Provider for creating global data for entire app

--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Components
+import App from './App';
 
 // Styles
 import './index.css';
 
-const App = () => {
-  return <></>;
-};
+// Global context
+export const TreesContext = createContext();
+
+const trees = [
+  { id: 1, type: 'Fir' },
+  { id: 2, type: 'Oak' },
+  { id: 3, type: 'Mahogany' },
+  { id: 4, type: 'Teak' },
+];
+
+const log = [
+  { id: 1, type: 'Fir log' },
+  { id: 2, type: 'Oak log' },
+  { id: 3, type: 'Mahogany log' },
+  { id: 4, type: 'Teak log' },
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+  <TreesContext.Provider value={{ trees, log }}>
+    <App />
+  </TreesContext.Provider>
+);
