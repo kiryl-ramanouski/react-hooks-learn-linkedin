de6e8fb6fd00e9cb5b37ccf592e04d132f805707
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-02 10:45:44 +0300

    feat: use custom useFetch() hook for fetching data from api/github.com. Use data &&, Object.keys(data)

--- react-hooks-app/src/App.js
+++ react-hooks-app/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
+// Custom hooks
+import useFetch from './useFetch';
+
 // Styles
 import './App.css';
 
-const App = () => {
-  return <></>;
+const App = ({ login }) => {
+  const { loading, data, error } = useFetch(
+    `https://api.github.com/users/${login}`
+  );
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  }
+  return (
+    <>
+      {data && (
+        <ul>
+          {Object.keys(data).map((key, i) => {
+            return <li key={i}>{`${key}: ${data[key]}`}</li>;
+          })}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default App;
