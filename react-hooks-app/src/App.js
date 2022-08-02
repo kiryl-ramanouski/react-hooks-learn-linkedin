0ac74845f5d7d33c3c7ab503c698104aa1067133
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-02 09:27:08 +0300

    feat: clear app

--- react-hooks-app/src/App.js
+++ react-hooks-app/src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
 
-// Custom hooks
-import { useTrees } from './index';
-
 // Styles
 import './App.css';
 
 const App = () => {
-  const { trees, log } = useTrees();
-  return (
-    <>
-      <h1 style={{ color: 'red', textAlign: 'center' }}>My trees</h1>
-      <ul>
-        {trees.map((item) => {
-          return <li key={item.id}>{item.type}</li>;
-        })}
-      </ul>
-      <h2 style={{ color: 'red', textAlign: 'center' }}>Use it to get log:</h2>
-      <ul>
-        {log.map((item) => {
-          return <li key={item.id}>{item.type}</li>;
-        })}
-      </ul>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
--- react-hooks-app/src/index.js
+++ react-hooks-app/src/index.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Components
@@ -7,32 +7,6 @@ import App from './App';
 // Styles
 import './index.css';
 
-// Global context
-const TreesContext = createContext();
-
-export const useTrees = () => {
-  const { trees, log } = useContext(TreesContext);
-  return { trees, log };
-};
-
-const trees = [
-  { id: 1, type: 'Fir' },
-  { id: 2, type: 'Oak' },
-  { id: 3, type: 'Mahogany' },
-  { id: 4, type: 'Teak' },
-];
-
-const log = [
-  { id: 1, type: 'Fir log' },
-  { id: 2, type: 'Oak log' },
-  { id: 3, type: 'Mahogany log' },
-  { id: 4, type: 'Teak log' },
-];
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <TreesContext.Provider value={{ trees, log }}>
-    <App />
-  </TreesContext.Provider>
-);
+root.render(<App />);
